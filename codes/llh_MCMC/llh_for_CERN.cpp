7789c34d08e883bf5da847b1dfb0c1be3fb8227b
Oliver Dahme <o.dahme@gmail.com>
2016-08-04 15:41:52 +0200

    ver 0.3
    llh running fine

--- codes/llh_MCMC/llh_for_CERN.cpp
+++ codes/llh_MCMC/llh_for_CERN.cpp
@@ -21,6 +21,7 @@ Likehood fit for CERN
 #include "TDecompChol.h"
 #include "assert.h"
 #include "TGraph.h"
+#include <unistd.h>
 using namespace std;
 
 
@@ -39,7 +40,7 @@ double f_gllh(double mu, double sigma, TVectorD gauss_points)
     calc += log(TMath::Gaus(gauss_points[i],mu,sigma,true));
   }
 
-  return calc;
+  return -calc;
 }
 
 
@@ -51,28 +52,37 @@ TRandom3 *rnd = new TRandom3(35);
 TApplication theApp("demoApplication",&argc,argv);
 //create a canvas
 
-TCanvas c1("c1","c1",1,1,1024,768);
+TCanvas c1("c1","c1",1,1,1920,1080);
 //TCanvas c2("c2","c2",1,1,1024,768);
 //TCanvas c3("c3","c3",1,1,1024,768);
 
+TH1F * hist = new TH1F("hist", "hist", 100, -2, 8);
 
 
 //simulate gauss distributed points
 
-unsigned long gp = 100; //number of points
+unsigned long gp = 1000; //number of points
 TVectorD gauss_points(gp);
 double real_sigma = 1.5;
 double real_mu = 3;
 for (unsigned int j = 0; j < gauss_points.GetNrows(); j++) {
   gauss_points[j] = rnd->Gaus(real_mu,real_sigma);
+  hist->Fill(gauss_points[j]);
 }
 
 //initialise
 
 unsigned int nparams = 2; //number of parameters
-unsigned int nstat = 30;//number of tries
+unsigned int nstat = 1000;//number of tries
+double maxstep = 0.01; //maximum step size
+double ubmu = 5; //upper bound for mu
+double lbmu = 1; //lower bound for mu
+double ubsigma = 3; //upper bound for sigma
+double lbsigma = 0.5; //lower bound for sigma
+double alphastar = 0.234; //forced acceptance rate
 
-ofstream mystream;
+ofstream rejectstream;
+ofstream acceptstream;
 
 Bool_t accepted;
 Bool_t inchain;
@@ -81,6 +91,14 @@ unsigned int naccepted = 0;
 
 TVectorD last(nparams);
 TVectorD curr(nparams);
+
+unsigned int nplotp = 50; //number of points in plot
+double llh_p[nstat];
+double mu_p[nstat];
+double minllh = 1e32;
+
+unsigned int nlast = 200;
+std::vector<bool> lastaccepted;
 
 TMatrixDSym identity(nparams);
 identity.UnitMatrix();
@@ -102,37 +120,96 @@ last[1] = first_simga;
 
 //MCMC
 
-double alphastar = 0.234; //forced acceptance rate
 
-double llh_p[nstat];
-double mu_p[nstat];
-
+rejectstream.open("list_of_rejected_candidates.txt");
+acceptstream.open("List_of_accepted_candidates.txt");
 for (unsigned int i = 0; i < nstat; i++) {
   curr = last; //use value of last for current then vary
 
   TVectorD WN(nparams);
   for (unsigned int j = 0; j < WN.GetNrows() ; j++) {
-    WN[j] = rnd->Gaus(0.0, 1.0);
+    WN[j] = rnd->Gaus(0.0, maxstep);
   }
   TVectorD SW(SNminusone*WN);
   curr += SW;
 
-std::cout << "mu = "<< curr[0] << std::endl;
-std::cout << "sigma = "<< curr[1] << std::endl;
+  if (curr[0] < lbmu) {
+    curr[0] = lbmu;
+  }
+  if (curr[0] > ubmu) {
+    curr[0] = ubmu;
+  }
+  if (curr[1] < lbsigma) {
+    curr[1] = lbsigma;
+  }
+  if (curr[1] > ubsigma) {
+  curr[1] = ubsigma;
+  }
+
+
+
+
+// std::cout << "sigma = "<< curr[1] << std::endl;
 
   //get llh for current and last
   double llh_last = f_gllh(last[0],last[1],gauss_points);
   double llh_curr = f_gllh(curr[0],curr[1],gauss_points);
 
-  std::cout << "llh = " << llh_curr << std::endl;
+if (llh_curr < minllh) {
+  minllh = llh_curr;
+}
+
   llh_p[i] = llh_curr;
   mu_p[i] = curr[0];
 
-  double alpha = std::min(1.0, exp(abs(llh_curr - llh_last)));
-  std::cout << "alpha = " << alpha << std::endl;
+//plotting everz 50th point of the llh
+/*
+if (i % (nstat/nplotp) == 0) {
+  double llhplotp[inplotp+1];
+  double muplotp[inplotp+1];
+  std::cout << "mu = "<< curr[0] << std::endl;
+  std::cout << "llh = " << llh_curr << std::endl;
+  llh_p[inplotp] = llh_curr;
+  mu_p[inplotp] = curr[0];
+  for (unsigned int j = 0; j < inplotp; j++) {
+    llhplotp[j] = llh_p[j];
+    muplotp[j] = mu_p[j];
+  }
+
+  TGraph * gr = new TGraph(inplotp,muplotp,llhplotp);
+  c1.cd();
+  gr->Draw("A*");
+  c1.Update();
+  inplotp++;
+  usleep(1000000);
+}*/
+
+//evolution of the llh
+
+  if (i > nplotp) {
+    double llhplotp[nplotp];
+    double muplotp[nplotp];
+    for (unsigned int inplotp = 0; inplotp < nplotp; inplotp++) {
+      llhplotp[inplotp] = llh_p[i-inplotp];
+      muplotp[inplotp] = mu_p[i-inplotp];
+    }
+
+
+    TGraph * gr = new TGraph(nplotp,muplotp,llhplotp);
+    c1.cd();
+    gr->Draw("A*");
+    c1.Update();
+    //usleep(500);
+  }
+
+
+
+  double alpha = std::min(1.0, exp(llh_last - llh_curr));
+//  std::cout << "alpha = " << alpha << std::endl;
   double r = rnd->Uniform(0,1);
   accepted = false;
   double acceptrate = double(naccepted)/double(ntested);
+
 
   if (r < alpha) {
     accepted = true;
@@ -140,24 +217,48 @@ std::cout << "sigma = "<< curr[1] << std::endl;
     //success, candidate filled below
     last = curr;
   } else {
+    //std::cout << "candidate found" << std::endl;
     accepted = false;
     inchain = false;
     //first save rejected candidate to file
-    mystream.open("list_of_candidates.txt");
     for (unsigned int line = 0; line < curr.GetNrows(); line++) {
-      mystream << curr[i] << "\n";
+      rejectstream << curr[line] << "\n";
+      //std::cout << curr[line] << std::endl;
     }
-    mystream.close();
     //then reset to last candidate
     curr = last;
   }
 
-
   inchain = true;
 
+  for (unsigned int line = 0; line < curr.GetNrows(); line++) {
+    acceptstream << curr[line] << "\n";
+    //std::cout << curr[line] << std::endl;
+  }
 
 
 
+  lastaccepted.insert(lastaccepted.begin(), accepted);
+  double lastratio = 0.0;
+  if (lastaccepted.size() > nlast)
+  {
+  //lastaccepted.push_back(accepted);
+  unsigned int nlastaccepted = 0;
+  for (unsigned int j=0; j<lastaccepted.size(); j++)
+  if (lastaccepted.at(j))
+    nlastaccepted++;
+  lastratio = double(nlastaccepted)/double(lastaccepted.size());
+  lastaccepted.pop_back();
+  }
+
+  if (accepted){
+    std::cout << "set " << i << " accepted, rate " << std::fixed << std::setprecision(3) << acceptrate << " last " << nlast << " " << lastratio << std::endl;
+  }
+  else{
+    std::cout << "set " << i << " rejected, rate " << std::fixed << std::setprecision(3) << acceptrate << " last " << nlast << " " << lastratio << std::endl;
+  }
+
+  ntested++;
 
   //update S matrix
   TMatrixDSym SNminusoneT(SNminusone);
@@ -187,14 +288,34 @@ std::cout << "sigma = "<< curr[1] << std::endl;
   }
 }
 
+rejectstream.close();
+acceptstream.close();
 std::cout << ntested << " points were tested" << std::endl;
 std::cout << naccepted << " points were accepted" << std::endl;
 std::cout << "The accept fraction is " << double(naccepted)/double(ntested) << std::endl;
 
+double mue[2];
+unsigned int imu = 0;
+for (unsigned int i = 0; i < nstat; i++) {
+  llh_p[i] -= minllh;
+  if (llh_p[i] < 0.51 && llh_p[i] > 0.49) {
+    std::cout << "mu = "<< mu_p[i] << std::endl;
+    mue[imu] = mu_p[i];
+    imu++;
+  }
+}
+
+std::cout << "error at 68p = " << abs(mue[0] - mue[1]) << std::endl;
+
 
 TGraph * gr = new TGraph(nstat,mu_p,llh_p);
 c1.cd();
-gr->Draw("A*");
+//gr->Draw("A*");
+c1.SaveAs("lastllh.png");
+//c2.cd();
+//hist->Fit("gaus");
+//hist->Draw();
+//c2.SaveAs("gauss_points.png");
 
 //turns off the program with mous clic
 theApp.Connect("TCanvas","Closed()","TApplication",&theApp,"Terminate()");
--- codes/llh_MCMC/llh_scan/llh_scan.cpp
+++ codes/llh_MCMC/llh_scan/llh_scan.cpp
@@ -44,46 +44,53 @@ TRandom3 *rnd = new TRandom3(35);
 unsigned long gp = 1000; //number of points
 TVectorD gauss_points(gp);
 const double real_sigma = 1.5;
-const double real_mu = 3;
+const double real_mu = 3.0;
 for (unsigned int j = 0; j < gauss_points.GetNrows(); j++) {
   gauss_points[j] = rnd->Gaus(real_mu,real_sigma);
 }
 
 
-unsigned int count = 500;
+unsigned int count = 50000;
 double llh_p[count];
-double llh_p2[count];
 double mu_p[count];
-double sigma_p[count];
 
 double sigma = 1.5;
 double mu = 3;
 
+double llhmin = 1e32;
+double calc = 0;
 for (unsigned int i = 1; i < count+1; i++) {
-  mu = i/100.0;
-  double calc = f_gllh(mu,sigma,gauss_points);
+  mu = i/10000.0;
+  calc = f_gllh(mu,sigma,gauss_points);
   mu_p[i] = mu;
-  std::cout << "mu = "<< mu << std::endl;
+  // std::cout << "mu = "<< mu << std::endl;
   llh_p[i] = calc;
+  if (calc < llhmin) {
+    llhmin = calc;
+  }
 }
 
 mu = 3;
+unsigned int sigma_count = 300;
+double sigma_p[sigma_count];
+double llh_p2[sigma_count];
 
-for (unsigned int i = 15; i < count+15; i++) {
+
+for (unsigned int i = 20; i < sigma_count+20; i++) {
   sigma = i/100.0;
-  double calc = f_gllh(mu,sigma,gauss_points);
+  calc = f_gllh(mu,sigma,gauss_points);
   sigma_p[i] = sigma;
-  std::cout << "sigma = "<< sigma << std::endl;
-  std::cout << "llhs = "<< calc << std::endl;
+  // std::cout << "sigma = "<< sigma << std::endl;
+  // std::cout << "llhs = "<< calc << std::endl;
   llh_p2[i] = calc;
 }
 
 
 
-
+std::cout << "llhmin = "<< llhmin << std::endl;
 
 TGraph * gr = new TGraph(count,mu_p,llh_p);
-TGraph * gr2 = new TGraph(count,sigma_p,llh_p2);
+TGraph * gr2 = new TGraph(sigma_count,sigma_p,llh_p2);
 c1.cd();
 gr->Draw("A*");
 c1.SaveAs("lastmu.png");
